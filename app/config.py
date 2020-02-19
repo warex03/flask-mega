33950e5643d18e10afbb2a320ccc6be8b483fc0f
Arvin Dino <warex03@gmail.com>
2020-02-20 06:31:34 +0800

    Add models and migrations

--- app/config.py
+++ app/config.py
@@ -2,3 +2,5 @@ from os import environ
 
 class Config(object):
     SECRET_KEY = environ.get('SECRET_KEY') or 'super-long-secret-keyyyy'
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL') or 'postgresql+psycopg2://postgres:postgres@localhost:5432/megatut'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
