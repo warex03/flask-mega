ea84f13eaaf21722c4be9f13eb5596923486f1ae
Arvin Dino <warex03@gmail.com>
2020-03-15 02:19:10 +0800

    Add flask-moment

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
