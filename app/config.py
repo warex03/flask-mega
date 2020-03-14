e7a72d3071e7822b84331a558d5291362203234b
Arvin Dino <warex03@gmail.com>
2020-03-15 00:53:26 +0800

    Implement pagination to posts

--- app/config.py
+++ app/config.py
@@ -4,3 +4,6 @@ class Config(object):
     SECRET_KEY = environ.get('SECRET_KEY') or 'super-long-secret-keyyyy'
     SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL') or 'postgresql+psycopg2://postgres:postgres@localhost:5432/megatut'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    ## app-specific
+    POSTS_PER_PAGE = 25
--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,8 @@ from werkzeug.urls import url_parse
 from flask_login import current_user, login_user, logout_user, login_required
 
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
-from app.models import User
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
+from app.models import User, Post
 
 @app.before_request
 def before_request():
@@ -13,21 +13,34 @@ def before_request():
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    posts = [
-        {
-            'author': {'username': 'Roan'},
-            'body': 'Matulog ka na babe'
-        },
-        {
-            'author': {'username': 'Noki'},
-            'body': 'Ayoko nga'
-        }
-    ]
-    return render_template('index.html', title='Home', posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html', title='Home', form=form, next_url=next_url,
+                           prev_url=prev_url, posts=posts.items)
+
+
+@app.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html', title='Explore', next_url=next_url, prev_url=prev_url,
+                           posts=posts.items)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -51,10 +64,12 @@ def login():
 
     return render_template('login.html', title='Sign In', form=form)
 
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -72,15 +87,18 @@ def register():
     
     return render_template('register.html', title='Register', form=form)
 
+
 @app.route('/user/<username>')
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) if posts.has_prev else None
+    return render_template('user.html', user=user, next_url=next_url, prev_url=prev_url,
+                           posts=posts.items)
+
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
@@ -97,6 +115,7 @@ def edit_profile():
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
 
+
 @app.route('/follow/<username>')
 def follow(username):
     user = User.query.filter_by(username=username).first()
@@ -110,6 +129,7 @@ def follow(username):
     db.session.commit()
     flash(f'You are following {username}')
     return redirect(url_for('user', username=username))
+
 
 @app.route('/unfollow/<username>')
 def unfollow(username):
